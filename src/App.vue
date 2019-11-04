fa622ba4fe82ad5e91e7170a22bc99dd859b84a1
nancy618 <486824356@qq.com>
2019-11-04 15:50:54 +0800

    修改App-vue

--- src/App.vue
+++ src/App.vue
@@ -18,5 +18,39 @@ export default {
 </script>
 
 <style>
+  .box {
+    width: 600px;
+    height: 400px;
+    border: 2px solid gray ;
+    padding: 10px 5px 8px 15px;
+    position: absolute;
+    top: 50%;
+    left: 45%;
+    margin-left: -250px;
+    margin-top: -200px;
 
+  }
+  h2{
+    text-align: center;
+  }
+
+  .content {
+    width: 400px;
+    height: 40px;
+    margin: 20px auto;
+  }
+
+  .input1 {
+    margin-bottom: 20px;
+    width: 400px;
+    height: 40px;
+  }
+
+  .btn1 {
+    float: right;
+  }
+
+  .content li {
+    line-height: 40px;
+  }
 </style>
