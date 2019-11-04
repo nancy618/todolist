f521ad268dad09875898dc429e4f0f21665636e8
nancy618 <486824356@qq.com>
2019-11-04 15:44:54 +0800

    修改App-vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-
+    <div class="box">
+      <h2>&nbsp;待&nbsp;办&nbsp;事&nbsp;项</h2>
+      <div class="content">
+        <input class="input1" type="text" >
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
